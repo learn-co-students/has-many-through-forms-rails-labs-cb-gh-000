39cf37f4e5135677cb748d6003b9fc174ddba2d7
Ahmad Abdel-Halim <ahmadabdelhalimmoustafa@gmail.com>
2017-04-04 22:58:28 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 
   def index
@@ -9,6 +10,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.categories.build
   end
 
   def create
